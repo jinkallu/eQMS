aa2c5baa0be1a3699bc77f124b13e6e834c03fb5
KS Vijeesh <vijeesh.ks@gmail.com>
2023-08-03 05:29:48 +0000

    Merged PR 133: resolved merge conflicts
    Related work items: #497

--- src/components/SidebarListItem.tsx
+++ src/components/SidebarListItem.tsx
@@ -1,22 +1,92 @@
-import { Chip, ListItem, ListItemButton, ListItemText } from "@mui/material";
+import {
+  Box,
+  Chip,
+  Collapse,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+} from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
 import { useGetRepoDetails } from "../zustand/store";
-import { useNavigate } from "react-router";
+// import { useNavigate } from "react-router";
+import React from "react";
+import { markedToHtml } from "../utils/markedHelper";
+import { createSearchParams, useNavigate } from "react-router-dom";
 
 export default function SidebarListItem({ type, label }) {
-  const branchTypes = useGetRepoDetails((state) => state.branchTypes);
+  const {
+    branchTypes,
+    setFileNames,
+    repository,
+    branchFileNames,
+    setFileContent,
+  } = useGetRepoDetails((state) => state);
+  const [open, setOpen] = React.useState(false);
+  const [contentHtml, setContentHtml] = React.useState("");
   const navigate = useNavigate();
+
+  React.useEffect(() => {
+    if (repository && repository?.id) {
+      branchTypes[type]?.map((branch) => {
+        setFileNames(repository?.id, branch.name, type);
+      });
+    }
+  }, [repository, branchTypes]);
+
+  async function handleItemClick(branch) {
+    await setFileContent(
+      repository?.id,
+      `/qms/${branch.type}/${branch.relativePath}/${branch.relativePath}.md`,
+      branch.name,
+      branch.objectId
+    );
+
+    navigate({
+      pathname: "content/",
+      search: `?${createSearchParams({
+        objectId: branch.objectId,
+      })}`,
+    });
+  }
+
   return (
-    <ListItem disablePadding>
-      <ListItemButton>
-        <ListItemText primary={label}></ListItemText>
-      </ListItemButton>
-      <Chip
-        label={branchTypes && branchTypes[type]?.length}
-        color="success"
-        variant="outlined"
-      ></Chip>
-      <AddIcon onClick={() => navigate(`${type}crud`)}></AddIcon>
-    </ListItem>
+    <>
+      <ListItem
+        id={type}
+        disablePadding
+        onClick={() => setOpen((prev) => !prev)}
+      >
+        <ListItemButton>
+          <ListItemText primary={label}></ListItemText>
+        </ListItemButton>
+        <Chip
+          label={branchTypes && branchTypes[type]?.length}
+          color="success"
+          variant="outlined"
+        ></Chip>
+        <AddIcon onClick={() => navigate(`${type}crud`)}></AddIcon>
+        {open ? <ExpandLess /> : <ExpandMore />}
+      </ListItem>
+      <Collapse in={open} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+          {branchFileNames
+            ?.filter((branch) => branch.type === type)
+            ?.map((branch) => {
+              return (
+                <ListItemButton
+                  key={branch.objectId}
+                  sx={{ pl: 4 }}
+                  onClick={() => handleItemClick(branch)}
+                >
+                  <ListItemText primary={branch.relativePath} />
+                </ListItemButton>
+              );
+            })}
+        </List>
+      </Collapse>
+    </>
   );
 }
--- src/zustand/store.js
+++ src/zustand/store.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 
 import { getClient } from "azure-devops-extension-api/Common";
 import { GitRestClient } from "azure-devops-extension-api/Git";
-
+import { markedToHtml } from "../utils/markedHelper";
 export const useDynamicIsland = create((set) => ({
   open: false,
   message: "",
@@ -23,9 +23,11 @@ export const useProject = create((set) => ({
 
 export const useGetRepoDetails = create((set, get) => ({
   repository: {},
-  types: ["qm", "sop", "prod", "temp"],
+  types: ["qm", "sop", "prod", "temp", "database"],
   branchTypes: {},
   branches: [],
+  branchFileNames: [],
+  htmlContents: {},
   setRepository: async (projectId, repoName) => {
     try {
       const gitClient = getClient(GitRestClient);
@@ -49,6 +51,7 @@ export const useGetRepoDetails = create((set, get) => ({
       const gitClient = getClient(GitRestClient);
       const branches = await gitClient.getBranches(repositoryId);
       set((state) => ({ branches: branches || [] }));
+      const newBranchTypes = [];
       const typeBranchData = {};
       get().types?.map((type) => {
         typeBranchData[type] =
@@ -57,12 +60,90 @@ export const useGetRepoDetails = create((set, get) => ({
               const nameArray = branch.name.split("/");
               return nameArray[1] === type && nameArray[3] === "main";
             })
-            ?.map((item) => item.name) || [];
+            ?.map((item) => ({ name: item.name })) || [];
       });
       set((state) => ({ branchTypes: typeBranchData }));
     } catch (error) {
       console.error("Error fetching repository ID:", error);
       set((state) => ({ branches: [] }));
+    }
+  },
+
+  setFileNames: async (repositoryId, branchName, type) => {
+    const versionDescriptor = {
+      version: branchName,
+      versionType: 0,
+    };
+    try {
+      const gitClient = getClient(GitRestClient);
+
+      const item = await gitClient.getItem(
+        repositoryId,
+        `/qms/${type}`,
+        null, // project
+        null, // scopepath
+        0, // recursionLevel
+        undefined, // includeContentMetadata,
+        undefined, // latestProcessedChange
+        false, // download
+        versionDescriptor
+      );
+      if (item) {
+        const tree = await gitClient.getTree(
+          repositoryId,
+          item.objectId,
+          null,
+          undefined, // scopepath
+          true, // recursionLevel
+          undefined // includeContentMetadata,
+        );
+        if (tree) {
+          const treeEntries = tree?.treeEntries;
+          const relativePath = treeEntries?.find(
+            (entry) => entry.gitObjectType === 2
+          )?.relativePath;
+          set((state) => ({
+            branchFileNames: [
+              ...state.branchFileNames,
+              {
+                type,
+                relativePath,
+                name: branchName,
+                objectId: item?.objectId,
+              },
+            ],
+          }));
+        }
+      }
+    } catch (e) {}
+  },
+  setFileContent: async (repositoryId, path, branchName, objectId) => {
+    const versionDescriptor = {
+      version: branchName,
+      versionType: 0,
+    };
+    try {
+      const gitClient = getClient(GitRestClient);
+
+      const content = await gitClient.getItemText(
+        repositoryId,
+        path,
+        null,
+        undefined, // scopepath
+        undefined, // recursionLevel
+        undefined, // includeContentMetadata,
+        true, // latestProcessedChange
+        false, // download
+        versionDescriptor
+      );
+      const html = await markedToHtml(content);
+      const newRes = { [objectId]: html };
+      set((state) => ({ htmlContents: { ...state.htmlContents, ...newRes } }));
+    } catch (e) {
+      console.log(e);
+      const newRes = { objectId: "" };
+
+      set((state) => ({ htmlContents: { ...state.htmlContents, ...newRes } }));
     }
   },
 }));
