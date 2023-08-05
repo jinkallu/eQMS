dcad94658eca31d753a8fb05abbd81554a5bf8d7
vijeeshks <48626484+vijeeshks@users.noreply.github.com>
2023-08-05 11:58:59 +0530

    edit repo shaping up

--- src/components/marked/MarkedEditView.tsx
+++ src/components/marked/MarkedEditView.tsx
@@ -1,48 +1,56 @@
-import React, { useState, useEffect, useRef } from 'react';
-import useMarkdToHTML from './useMarkdToHTML';
+import React, { useState, useEffect, useRef } from "react";
+import useMarkdToHTML from "./useMarkdToHTML";
 
 import MarkedEditor from "./MarkedEditor";
 import MarkedHTMLViewer from "./MarkedHTMLViewer";
-import DisplayTasksRenderer from './DisplayTasksRenderer';
+import { Box, Chip, Paper, Typography } from "@mui/material";
 
+export default function MarkedEditView({ inData }) {
+  const [inputText, setInputText] = useState(inData);
+  const [parsedHTML, setParsedHTML] = useState("");
 
+  const { loadingHTML, markdToCustom } = useMarkdToHTML();
 
+  useEffect(() => {
+    function parseMarkdown() {
+      if (inputText && inputText.trim() !== "") {
+        const childHTMLDOM = markdToCustom(inputText, "markedHTMLViewer");
+      }
+    }
 
-export default function MarkedEditView() {
-    const [inputText, setInputText] = useState('');
-    const [parsedHTML, setParsedHTML] = useState('');
+    // Call the parseMarkdown function whenever inputText changes
+    parseMarkdown();
+  }, [inputText]);
 
-    const { loadingHTML, markdToCustom } = useMarkdToHTML();
+  return (
+    <Box
+      id="MarkedEditView"
+      sx={{
+        display: "flex",
+        minHeight: "90vh",
+        flexGrow: 1,
+        padding: "5px",
+        gap: "5px",
+      }}
+    >
+      <Paper elevation={3} sx={{ flex: 1 }}>
+        <MarkedEditor inputText={inputText} setInputText={setInputText} />
+      </Paper>
 
-    useEffect(() => {
+      <Paper
+        elevation={3}
+        sx={{ flex: 1, display: "flex", flexDirection: "column" }}
+      >
+        <Chip label="HTML Viewer" color="primary"></Chip>
 
-        function parseMarkdown() {
-
-            if (inputText && inputText.trim() !== '') {
-                const childHTMLDOM = markdToCustom(inputText, 'markedHTMLViewer');
-            }
-        }
-
-        // Call the parseMarkdown function whenever inputText changes
-        parseMarkdown();
-    }, [inputText]);
-
-    return (
-        <div id="MarkedEditView" style={{ display: 'flex', height: '100vh' }}>
-            <div style={{ flex: 1 }}>
-                <MarkedEditor inputText={inputText} setInputText={setInputText} />
-            </div>
-
-            <div style={{ flex: 1, height: '100%' }}>
-                <h1>HTML Viewer</h1>
-                <div
-                    id="markedHTMLViewerP"
-                    style={{ height: '100%', padding: '16px', boxSizing: 'border-box' }}
-                >
-                    {/* Render the MarkedHTMLViewer component with the resolved HTML */}
-                    <MarkedHTMLViewer inputText={parsedHTML} />
-                </div>
-            </div>
-        </div>
-    );
+        <Box
+          id="markedHTMLViewerP"
+          sx={{ height: "100%", padding: "16px", boxSizing: "border-box" }}
+        >
+          {/* Render the MarkedHTMLViewer component with the resolved HTML */}
+          <MarkedHTMLViewer inputText={parsedHTML} />
+        </Box>
+      </Paper>
+    </Box>
+  );
 }
