bb6958769979f864c40794cda3a3983e60f0ec58
Jinesh Kallunkathariyil <jinesh.bond@gmail.com>
2023-11-28 21:45:20 +0100

    tree from nodes and edges

---
+++ src/components/Pages/useProcessSteps.tsx
@@ -0,0 +1,86 @@
+import { useState } from "react";
+
+const useProcessSteps = () => {
+    const [stepTree, setStepTree] = useState(null);
+
+    // find the step which has no parent (sourceid !== targetid)
+    const initialStep = (edges) => {
+        for(let i = 0; i < edges.length; i++){
+            const srcEdge = edges[i];
+            for (let j = 0; j < edges.length; j++){
+                const tgtEdge = edges[j];
+                if (srcEdge.source === tgtEdge.target){
+                    continue
+                }
+            }
+            return srcEdge.source;
+        }
+    }
+
+    const findStepWithId = (id, nodes) => {
+        for(let i = 0; i < nodes.length; i++){
+            if(nodes[i].id === id){
+                return nodes[i];
+            }
+        }
+    }
+
+    const findStepsWithSrcNodeId = (srcId, nodes, edges) => {
+        const edgesWithSrc = [];
+        for (let i = 0; i < edges.length; i++){
+            if(srcId === edges[i].source){
+                edgesWithSrc.push(edges[i]);
+            }
+        }
+
+        const nodesWithSrc = [];
+        for(let i = 0; i < edgesWithSrc.length; i++){
+            const tgtId = edgesWithSrc[i].target;
+
+            for(let j = 0; j < nodes.length; j++){
+                const nodeId = nodes[j].id;
+                if(nodeId === tgtId){
+                    nodesWithSrc.push(nodes[j]);
+                    break;
+                }
+            }
+        }
+
+        return nodesWithSrc;
+    }
+
+    const createTree = (step) => {
+        const tree = {
+            id: step.id,
+            // stepElm: stepElm,
+            // templateName: templateName,
+            // templateId,
+            children: [],
+          };
+          return tree;
+    }
+
+    const traverse = (id, nodes, edges, tree) => {
+        const srcNodes = findStepsWithSrcNodeId(id, nodes, edges);
+        console.log(srcNodes);
+        for(let i = 0; i < srcNodes.length; i++){
+            const childTree = createTree(srcNodes[i]);
+            const populatedChildTree = traverse(childTree.id, nodes, edges, childTree);
+            tree.children.push(populatedChildTree);
+        }
+
+        return tree;
+    }
+
+    const createStepTree = (nodes, edges) => {
+        const initStepId = initialStep(edges);
+        const step = findStepWithId(initStepId, nodes);
+        const tree = createTree(step);
+        const finalTree = traverse(tree.id, nodes, edges, tree);
+        setStepTree(finalTree);
+    }
+
+    return {stepTree, createStepTree};
+}
+
+export default useProcessSteps;
