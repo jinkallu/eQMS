b7ef6157153953ffb34ecab1c31c8bd8fd1d3c92
vijeeshks <48626484+vijeeshks@users.noreply.github.com>
2023-12-18 13:00:02 +0530

    state issue

--- src/components/marked/ReactCustomTags/TextInputTagView.tsx
+++ src/components/marked/ReactCustomTags/TextInputTagView.tsx
@@ -7,7 +7,7 @@ export default function TextInputTagView({
   handleChange,
 }) {
   function handleChangeFun(e) {
-    if(handleChange){
+    if (handleChange) {
       handleChange(id, e.target.value);
     }
   }
@@ -17,30 +17,37 @@ export default function TextInputTagView({
     case "first":
       //component = element.outerHTML;
       component = (
-        <input value={state[id] || val} onChange={handleChangeFun}></input>
+        <input
+          value={(state && state[id]) || val}
+          onChange={handleChangeFun}
+        ></input>
       );
 
       break;
     case "middle":
-        const rows = element.getAttribute("rows");
-        const cols = element.getAttribute("cols");
+      const rows = element.getAttribute("rows");
+      const cols = element.getAttribute("cols");
       component = (
-        <textarea value={state[id] || val} id= {element.id} cols = {cols} rows = {rows} onChange={handleChangeFun}></textarea>
+        <textarea
+          value={(state && state[id]) || val}
+          id={element.id}
+          cols={cols}
+          rows={rows}
+          onChange={handleChangeFun}
+        ></textarea>
       );
       break;
     case "last":
-      if(state){
-        if(state[id]){
-          component = <span>{state[id]}</span>
-        }
-        else{
+      if (state) {
+        if (state[id]) {
+          component = <span>{state[id]}</span>;
+        } else {
           component = <span>{val}</span>;
         }
-      }
-      else{
+      } else {
         component = <span>{val}</span>;
       }
-      
+
       //component = <input value={state[id]} onChange={handleChangeFun}></input>;
 
       break;
