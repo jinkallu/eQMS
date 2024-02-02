715bbd742a41c9186538487701fc714737fa83c7
vijeeshks <48626484+vijeeshks@users.noreply.github.com>
2024-02-02 20:09:42 +0530

    r input added

--- src/components/marked/ReactCustomTags/TiptapEditor.tsx
+++ src/components/marked/ReactCustomTags/TiptapEditor.tsx
@@ -1,10 +1,15 @@
-import { EditorProvider, FloatingMenu, BubbleMenu, useCurrentEditor } from "@tiptap/react";
+import {
+  EditorProvider,
+  FloatingMenu,
+  BubbleMenu,
+  useCurrentEditor,
+} from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import { useExtnStore } from "../../../zustand/store";
 import { customTableNode } from "./CustomTableNode";
 import CustomInputNode from "./CustomInputNode";
-
-
+import { TiptapMenuBar } from "./TiptapMenubar";
+import CustomInputReact from "./CustomInputReactExtension";
 
 // define your extension array
 const extensions = [
@@ -13,35 +18,37 @@ const extensions = [
   customTableNode.table,
   customTableNode.table_row,
   customTableNode.table_cell,
+  CustomInputReact,
 ];
 
 // const content = "<p>Hello World!</p>";
 
 const TiptapEditor = ({ id }) => {
   const { templateState } = useExtnStore((state) => state);
-  
 
-  
   return (
-    <EditorProvider extensions={extensions} content={templateState[id] || " "} slotBefore={<InsertCustomInputButton />}>
-      <FloatingMenu>
-      
-
-      </FloatingMenu>
-      <BubbleMenu>This is the bubble menu</BubbleMenu>
+    <EditorProvider
+      extensions={extensions}
+      content={templateState[id] || " "}
+      slotBefore={<TiptapMenuBar />}
+    >
+      {""}
     </EditorProvider>
   );
 };
 
 const InsertCustomInputButton = () => {
-  const {editor} = useCurrentEditor();
+  const { editor } = useCurrentEditor();
 
   const insertCustomInput = () => {
-    editor.chain().focus().insertContent({type: "custom_input", attrs: { id: "testInput"}}).run();
+    editor
+      .chain()
+      .focus()
+      .insertContent({ type: "custom_input", attrs: { id: "testInput" } })
+      .run();
     //const node = editor.schema.nodes.custom_input.create({ id: 'testInput' });
     //console.log(node);
     //editor.chain().focus().insertContent(node).run();
-
   };
 
   return <button onClick={insertCustomInput}>Insert Custom Input</button>;
---
+++ src/components/marked/ReactCustomTags/TiptapMenubar.tsx
@@ -0,0 +1,229 @@
+import { useCurrentEditor } from "@tiptap/react";
+import TiptapInputDialog from "./TiptapInputDialog";
+import { useState } from "react";
+export function TiptapMenuBar() {
+  const { editor } = useCurrentEditor();
+  const [inputOpen, setInputOpen] = useState(false);
+
+  if (!editor) {
+    return null;
+  }
+  function handleCreateInputClick() {
+    setInputOpen(true);
+  }
+
+  function addInput(id) {
+    console.log("id", id);
+    editor
+      .chain()
+      .focus()
+      .insertContent({
+        type: "rInput",
+        attrs: { id },
+      })
+      .run();
+
+    setInputOpen(false);
+  }
+
+  return (
+    <>
+      <TiptapInputDialog
+        open={inputOpen}
+        setOpen={setInputOpen}
+        addInput={addInput}
+      ></TiptapInputDialog>
+      <button
+        onClick={() => editor.chain().focus().toggleBold().run()}
+        disabled={!editor.can().chain().focus().toggleBold().run()}
+        className={editor.isActive("bold") ? "is-active" : ""}
+      >
+        bold
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleItalic().run()}
+        disabled={!editor.can().chain().focus().toggleItalic().run()}
+        className={editor.isActive("italic") ? "is-active" : ""}
+      >
+        italic
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleStrike().run()}
+        disabled={!editor.can().chain().focus().toggleStrike().run()}
+        className={editor.isActive("strike") ? "is-active" : ""}
+      >
+        strike
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleCode().run()}
+        disabled={!editor.can().chain().focus().toggleCode().run()}
+        className={editor.isActive("code") ? "is-active" : ""}
+      >
+        code
+      </button>
+      <button onClick={() => editor.chain().focus().unsetAllMarks().run()}>
+        clear marks
+      </button>
+      <button onClick={() => editor.chain().focus().clearNodes().run()}>
+        clear nodes
+      </button>
+      <button
+        onClick={() => editor.chain().focus().setParagraph().run()}
+        className={editor.isActive("paragraph") ? "is-active" : ""}
+      >
+        paragraph
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+        className={editor.isActive("heading", { level: 1 }) ? "is-active" : ""}
+      >
+        h1
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+        className={editor.isActive("heading", { level: 2 }) ? "is-active" : ""}
+      >
+        h2
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
+        className={editor.isActive("heading", { level: 3 }) ? "is-active" : ""}
+      >
+        h3
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
+        className={editor.isActive("heading", { level: 4 }) ? "is-active" : ""}
+      >
+        h4
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 5 }).run()}
+        className={editor.isActive("heading", { level: 5 }) ? "is-active" : ""}
+      >
+        h5
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 6 }).run()}
+        className={editor.isActive("heading", { level: 6 }) ? "is-active" : ""}
+      >
+        h6
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleBulletList().run()}
+        className={editor.isActive("bulletList") ? "is-active" : ""}
+      >
+        bullet list
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleOrderedList().run()}
+        className={editor.isActive("orderedList") ? "is-active" : ""}
+      >
+        ordered list
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleCodeBlock().run()}
+        className={editor.isActive("codeBlock") ? "is-active" : ""}
+      >
+        code block
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleBlockquote().run()}
+        className={editor.isActive("blockquote") ? "is-active" : ""}
+      >
+        blockquote
+      </button>
+      <button onClick={() => editor.chain().focus().setHorizontalRule().run()}>
+        horizontal rule
+      </button>
+      <button onClick={() => editor.chain().focus().setHardBreak().run()}>
+        hard break
+      </button>
+      <button
+        onClick={() => editor.chain().focus().undo().run()}
+        disabled={!editor.can().chain().focus().undo().run()}
+      >
+        undo
+      </button>
+      <button
+        onClick={() => editor.chain().focus().redo().run()}
+        disabled={!editor.can().chain().focus().redo().run()}
+      >
+        redo
+      </button>
+
+      <button
+        onClick={handleCreateInputClick}
+        className={
+          editor.isActive("textStyle", { color: "#958DF1" }) ? "is-active" : ""
+        }
+      >
+        Input
+      </button>
+
+      {/* <button
+        onClick={() =>
+          editor
+            .chain()
+            .focus()
+            .insertContent({
+              type: "rTable",
+              //   content: [
+              //     {
+              //       type: "text",
+              //       text: "New block added",
+              //     },
+              //   ],
+            })
+            .run()
+        }
+        className={
+          editor.isActive("textStyle", { color: "#958DF1" }) ? "is-active" : ""
+        }
+      >
+        Table
+      </button> */}
+
+      {/* <button
+        onClick={() =>
+          editor
+            .chain()
+            .focus()
+            .insertContent({ type: "custom_input", attrs: { id: "testInput" } })
+            .run()
+        }
+        className={
+          editor.isActive("textStyle", { color: "#958DF1" }) ? "is-active" : ""
+        }
+      >
+        Table
+      </button> */}
+
+      <button
+        onClick={() => console.log(editor.getHTML())}
+        className={
+          editor.isActive("textStyle", { color: "#958DF1" }) ? "is-active" : ""
+        }
+      >
+        Console HTML
+      </button>
+      <button
+        onClick={() => console.log(editor.getJSON())}
+        className={
+          editor.isActive("textStyle", { color: "#958DF1" }) ? "is-active" : ""
+        }
+      >
+        Console JSON
+      </button>
+
+      <button
+        onClick={() => console.log(editor.getText())}
+        className={
+          editor.isActive("textStyle", { color: "#958DF1" }) ? "is-active" : ""
+        }
+      >
+        Console TEXT
+      </button>
+    </>
+  );
+}
